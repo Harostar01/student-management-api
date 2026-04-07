dfd03143f4dbb4b435fa982f5483080cd9be3cd2
Harostar01 <joshuaolamilekan008@gmail.com>
2026-04-07 15:17:05 +0100

    Initialize database table on app launch
    Add database table creation during app startup

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask
 from routes import student_routes
+from models import create_table
 
 app = Flask(__name__)
+
+create_table()  # create DB table
 
 app.register_blueprint(student_routes)
 
